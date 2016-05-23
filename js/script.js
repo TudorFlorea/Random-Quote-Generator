037e3e05ac2e4204d4bb9e284f5b187d25f51030
Tudor Florea <ftudorel92@yahoo.com>
2016-05-23 13:21:52 +0300

    Added new layout

--- js/script.js
+++ js/script.js
@@ -1,41 +1,37 @@
 $(document).ready(function () {
         
-//================== Generate the first random quote ===========================//
-        var randId = Math.round(Math.random() * 49 + 1);
-        var htmlQuotes = "";
-        var htmlImages= "";
-	  	var tweet = "";
+    //================== Generate the first random quote ===========================//
+        
         $.getJSON('js/quotes.json', function(json){
-			htmlQuotes = "";
-			htmlImages= "";
-			tweet = "";
+            var randId = Math.floor(Math.random() * json.length + 1);
+            console.log("json.length " + json.length);
+			var htmlQuotes = "";
+			var htmlImages= "";
+			var tweet = "";
 			console.log("randId is " + randId);
         	json = json.filter(function (val) {
           	return (val.id == randId);
+            console.log("json.length after filter" + json.length);
         });  
        json.forEach(function(val) {
-			htmlQuotes += "<p>" + val.Quote + "</p>";
+			htmlQuotes += "<p><i class='fa fa-quote-left'></i>" + val.Quote + "<i class='fa fa-quote-right' aria-hidden='true'></i></p>";
 			htmlQuotes += "<p> - " + val.Author + "</p>";
 			htmlImages += "<img class='img img-responsive' src='" + val.link + "'>";
-			tweet += 'https://twitter.com/intent/tweet?text=' + '"' + val.Quote + '"' + ' - ' + val.Author; 
+			tweet += 'https://twitter.com/intent/tweet?text=' + '"' + val.Quote + '"' + ' - ' + val.Author;
         }); // --- end .forEach ---
-			$("#quotes").html(htmlQuotes);
-			$("#images").html(htmlImages);
+            console.log("json.length after filter" + json.length);
+			$("#quotes").html(htmlQuotes); //Change quote text
+			$("#images").html(htmlImages); //Change quote image
 			$('#tweet').click(function() {
-				window.location.href = tweet;
+				window.open(tweet);
 				return false;
-				});
+				}); //Tweet onClicking button
 		});	// --- end .getJSON ---
         
 	//============= Generate a random quote when the button is pressed==============//
 	$("#newQuote").on("click", function(){
-		randId = Math.round(Math.random() * 49 + 1);
-        
-		//https://api.myjson.com/bins/4ywow
-		//https://api.myjson.com/bins/36xtu
-		//https://api.myjson.com/bins/25vqc
-		//https://api.myjson.com/bins/3b3ww
 		$.getJSON('js/quotes.json', function(json){
+            randId = Math.floor(Math.random() * json.length + 1);
 			htmlQuotes = "";
 			htmlImages = "";
 			tweet = "";
@@ -44,17 +40,22 @@ $(document).ready(function () {
           return (val.id == randId);
         });  
        json.forEach(function(val) {
-          htmlQuotes += "<p>" + val.Quote + "</p>";
+          htmlQuotes += "<p><i class='fa fa-quote-left'></i>" + val.Quote + "<i class='fa fa-quote-right' aria-hidden='true'></i></p>";
 			 htmlQuotes += "<p> - " + val.Author + "</p>";
 			 htmlImages += "<img class='img img-responsive' src='" + val.link + "'>";
 			 tweet += 'https://twitter.com/intent/tweet?text=' + '"' + val.Quote + '"' + ' - ' + val.Author;
         }); // --- end .forEach ---
-		$("#quotes").html(htmlQuotes);
-		$("#images").html(htmlImages);
+            
+        $("#quotes, #images").fadeOut(1500, function() {
+            $("#quotes").html(htmlQuotes); //Change quote text
+            $("#images").html(htmlImages); //Change quote image
+            $("#quotes, #images").fadeIn(1500);
+        }); // --- end the fadeOut/In of the Image and the quotes ---
+
 		$('#tweet').click(function() {
-				window.location.href = tweet;
+				window.open(tweet);
 				return false;
-				});
+				}); //Tweet onClicking button
 		});	// --- end .getJSON ---
 	}); // --- end .onClick ---
 }); // --- end document.ready ---
